f52d3b7db7be5a2c99268d65f1a8f768069cb89f
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-07-13 11:14:39 +0100

    ar(feat) sort it

--- lib/model/interfaces/index.ts
+++ lib/model/interfaces/index.ts
@@ -13,6 +13,10 @@ export { getUserMeta } from './mdb-get-interface';
 // write
 export { commitUpdate, initSignUpUser } from './mdb-update-interface';
 
+
+/* prisma-public-ifaces */
+export { default as GetPublicListings } from './get-public-listings-iface';
+
 /* rm */
 
 // read
