1e2d18f6d7d1fc50105439924df1a595debf329e
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-07-20 17:52:36 +0100

    ar preg nothing

--- lib/model/interfaces/index.ts
+++ lib/model/interfaces/index.ts
@@ -3,6 +3,22 @@
 /* prisma-public-ifaces */
 export { default as GetPublicListings } from './get-public-listings-iface';
 
+/* prisma-private-ifaces */
+
+// lib
+export { canI, whoAmI } from './middleware';
+
+// read
+export { default as GetPrivateCommonServices } from './get-private-common-services';
+export { default as GetPrivateServices } from './get-private-services';
+export { default as GetPrivateCommonAbilities } from './get-private-common-abilities';
+export { default as GetPrivateAbilities } from './get-private-abilities';
+
+// update
+export { default as UpdatePrivateUserServices } from './update-private-user-services';
+export { default as UpdatePrivateUserAbilities } from './update-private-user-abilities';
+export { default as UpdatePrivateUserFavoriteListings } from './update-private-user-favorite-listings';
+
 /* rm */
 
 // read
