08705abc53669889c8069cd9a0f835b504c77c40
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-01 14:04:52 +0100

    ar(fix) apple stuff

--- src/app/api/auth/callback/apple/route.ts
+++ src/app/api/auth/callback/apple/route.ts
@@ -6,6 +6,19 @@ import { GET as NextAuthGET } from '../../[...nextauth]/route';
 export { NextAuthGET as GET };
 
 export async function POST(req: NextRequest) {
+  const response = NextResponse.next();
+  const pkce = request.cookies.get('next-auth.pkce.code_verifier');
+
+  if (pkce?.value) {
+    response.cookies.set('next-auth.pkce.code_verifier', pkce.value, {
+      httpOnly: true,
+      sameSite: 'none',
+      path: '/',
+      secure: true,
+    });
+    console.log({ pkce, response, to: request.nextUrl.pathname });
+  }
+
   const data = await req.formData();
   const queryParams: { [key: string]: string } = {};
   data.forEach((value, key) => {
@@ -13,17 +26,15 @@ export async function POST(req: NextRequest) {
   });
 
   const searchParams = new URLSearchParams(queryParams);
-  const cookies = req.headers.get('Cookie') || req.cookies.toString() || '';
+  // const cookies = req.headers.get('Cookie') || req.cookies.toString() || '';
 
-  console.log({ cookies, data, queryParams, dest: req.headers.get('host') });
+  // console.log({ cookies, data, queryParams, dest: req.headers.get('host') });
 
   return NextResponse.redirect(
     `https://${req.headers.get('host')}/api/auth/callback/apple?${searchParams.toString()}`,
     {
       status: 302,
-      headers: {
-        Cookie: cookies,
-      },
+      ...response,
     },
   );
 }
