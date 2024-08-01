e644d7b4489689b97c318f2267f89d3b9d9f6b47
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-01 14:28:31 +0100

    ar(fix) apple stuff

--- src/app/api/auth/callback/apple/route.ts
+++ src/app/api/auth/callback/apple/route.ts
@@ -29,6 +29,7 @@ export async function POST(req: NextRequest) {
       path: '/',
       secure: true,
     });
+    response.headers.set('Cookie', response.cookies.toString());
     console.log({ pkce, response, to: req.nextUrl.pathname });
   }
   // const cookies = req.headers.get('Cookie') || req.cookies.toString() || '';
