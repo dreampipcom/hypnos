7666a0c4df6b5199d262f141105da7f7be575ae6
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-24 15:53:55 +0100

    ar(feat) [DPTM-8] Dockerize and GCP

--- lib/model/prisma-private-connector.ts
+++ lib/model/prisma-private-connector.ts
@@ -1,26 +1,25 @@
 import type { PrismaClient as PrivatePrismaType } from '@dreampipcom/db-private/prisma-client';
-// import { PrismaClient as PrivatePrisma } from '@dreampipcom/db-private/prisma-client/edge';
+import { PrismaClient as PrivatePrisma } from '@dreampipcom/db-private/prisma-client/edge';
 import { PrismaClient as PrivatePrismaStandalone } from '@dreampipcom/db-private/prisma-client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-// const prismaClientSingleton = () => {
-//   return new PrivatePrisma().$extends(withAccelerate());
-// };
+const prismaClientSingleton = () => {
+  return new PrivatePrisma().$extends(withAccelerate());
+};
 
 const prismaClientSingletonStandalone = () => {
   return new PrivatePrismaStandalone();
 };
 
 declare const globalThis: {
-  // prismaPrivateGlobal: PrivatePrismaType;
+  prismaPrivateGlobal: PrivatePrismaType;
   prismaPrivateGlobalStandalone: PrivatePrismaType;
 } & typeof global;
 
 const privatePrisma =
   process.env.NEXUS_STANDALONE === 'true'
     ? globalThis.prismaPrivateGlobalStandalone ?? (prismaClientSingletonStandalone() as unknown as PrivatePrismaType)
-    : globalThis.prismaPrivateGlobalStandalone ?? (prismaClientSingletonStandalone() as unknown as PrivatePrismaType);
-// : globalThis.prismaPrivateGlobal ?? (prismaClientSingleton() as unknown as PrivatePrismaType);
+    : globalThis.prismaPrivateGlobal ?? (prismaClientSingleton() as unknown as PrivatePrismaType);
 
 export default privatePrisma;
 
@@ -28,7 +27,6 @@ if (process.env.NODE_ENV !== 'production') {
   if (process.env.NEXUS_STANDALONE === 'true') {
     globalThis.prismaPrivateGlobalStandalone = privatePrisma;
   } else {
-    globalThis.prismaPrivateGlobalStandalone = privatePrisma;
-    // globalThis.prismaPrivateGlobal = privatePrisma;
+    globalThis.prismaPrivateGlobal = privatePrisma;
   }
 }
--- lib/model/prisma-public-connector.ts
+++ lib/model/prisma-public-connector.ts
@@ -1,26 +1,25 @@
 import type { PrismaClient as PublicPrismaType } from '@dreampipcom/db-public/prisma-client';
-// import { PrismaClient as PublicPrisma } from '@dreampipcom/db-public/prisma-client/edge';
+import { PrismaClient as PublicPrisma } from '@dreampipcom/db-public/prisma-client/edge';
 import { PrismaClient as PublicPrismaStandalone } from '@dreampipcom/db-public/prisma-client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-// const prismaClientSingleton = () => {
-//   return new PublicPrisma().$extends(withAccelerate());
-// };
+const prismaClientSingleton = () => {
+  return new PublicPrisma().$extends(withAccelerate());
+};
 
 const prismaClientSingletonStandalone = () => {
   return new PublicPrismaStandalone();
 };
 
 declare const globalThis: {
-  // prismaPublicGlobal: PublicPrismaType;
+  prismaPublicGlobal: PublicPrismaType;
   prismaPublicGlobalStandalone: PublicPrismaType;
 } & typeof global;
 
 const publicPrisma =
   process.env.NEXUS_STANDALONE === 'true'
     ? globalThis.prismaPublicGlobalStandalone ?? (prismaClientSingletonStandalone() as unknown as PublicPrismaType)
-    : globalThis.prismaPublicGlobalStandalone ?? (prismaClientSingletonStandalone() as unknown as PublicPrismaType);
-// : globalThis.prismaPublicGlobal ?? (prismaClientSingleton() as unknown as PublicPrismaType);
+    : globalThis.prismaPublicGlobal ?? (prismaClientSingleton() as unknown as PublicPrismaType);
 
 export default publicPrisma;
 
@@ -28,7 +27,6 @@ if (process.env.NODE_ENV !== 'production') {
   if (process.env.NEXUS_STANDALONE === 'true') {
     globalThis.prismaPublicGlobalStandalone = publicPrisma;
   } else {
-    globalThis.prismaPublicGlobalStandalone = publicPrisma;
-    // globalThis.prismaPublicGlobal = publicPrisma;
+    globalThis.prismaPublicGlobal = publicPrisma;
   }
 }
