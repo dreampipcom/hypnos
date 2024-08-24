5a0bab3bff58bedf050e7f6a69025b30bcefa243
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-24 12:45:42 +0100

    ar(feat) [DPTM-8] Dockerize and GCP

---
+++ postInstall.js
@@ -0,0 +1,12 @@
+import { execSync } from 'child_process';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (process.env.NEXUS_STANDALONE === 'true') {
+  console.log('--- RUNNING STANDALONE POSTINSTALL HOOK');
+  execSync('npx patch-package &&  npm run schema:generate:all:standalone');
+} else {
+  console.log('--- RUNNING EDGE POSTINSTALL HOOK');
+  execSync('npx patch-package &&  npm run schema:generate:all:standalone');
+}
