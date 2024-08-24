88cecaf6c70d6746338b381af1fa53aa2a3be96e
Angelo Reale <12191809+angeloreale@users.noreply.github.com>
2024-08-24 22:50:49 +0100

    ar(feat) [DPTM-8] [DPCP-88] [DPCP-81] [DPCP-118] Dockerize and GCP (#41)
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP
    
    * ar(feat) [DPTM-8] Dockerize and GCP

--- lib/model/prisma-private-connector.ts
+++ lib/model/prisma-private-connector.ts
@@ -1,16 +1,32 @@
+import type { PrismaClient as PrivatePrismaType } from '@dreampipcom/db-private/prisma-client';
 import { PrismaClient as PrivatePrisma } from '@dreampipcom/db-private/prisma-client/edge';
+import { PrismaClient as PrivatePrismaStandalone } from '@dreampipcom/db-private/prisma-client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prismaClientSingleton = () => {
   return new PrivatePrisma().$extends(withAccelerate());
 };
 
+const prismaClientSingletonStandalone = () => {
+  return new PrivatePrismaStandalone();
+};
+
 declare const globalThis: {
-  prismaPrivateGlobal: ReturnType<typeof prismaClientSingleton>;
+  prismaPrivateGlobal: PrivatePrismaType;
+  prismaPrivateGlobalStandalone: PrivatePrismaType;
 } & typeof global;
 
-const privatePrisma = globalThis.prismaPrivateGlobal ?? prismaClientSingleton();
+const privatePrisma =
+  process.env.NEXUS_STANDALONE === 'true'
+    ? globalThis.prismaPrivateGlobalStandalone ?? (prismaClientSingletonStandalone() as unknown as PrivatePrismaType)
+    : globalThis.prismaPrivateGlobal ?? (prismaClientSingleton() as unknown as PrivatePrismaType);
 
 export default privatePrisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaPrivateGlobal = privatePrisma;
+if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NEXUS_STANDALONE === 'true') {
+    globalThis.prismaPrivateGlobalStandalone = privatePrisma;
+  } else {
+    globalThis.prismaPrivateGlobal = privatePrisma;
+  }
+}
